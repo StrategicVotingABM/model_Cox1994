5ed6bad5fa0d13f52cc02c246b07194054747960
SamuelBaltz <sbaltz@umich.edu>
2018-03-14 22:44:31 -0400

    Update GlobalFuncs.py

--- source/GlobalFuncs.py
+++ source/GlobalFuncs.py
@@ -3,6 +3,7 @@
 #-----------------------------------------------------------------------------#
 import numpy as np
 import matplotlib as plt
+import scipy.stats
 
 #Wrapper function to generalize the generation of random preferences. Later
 #we can simply alter its implementation to handle different randomization of
@@ -26,14 +27,29 @@ def randUtilities(minPreference, maxPreference, nCandidates, distribution):
             return np.random.dirichlet((4,3,2,1), 1).transpose()
     elif distribution == "3dirichlets":
         #Sample 1-d policy preference from three dirichlet distributions:
-        coin = np.random.randint(0,2)
+        coin = np.random.randint(0,3)
         if coin == 0:
             return np.random.dirichlet((1,2,3,4), 1).transpose()
         elif coin == 1:
             return np.random.dirichlet((4,3,2,1), 1).transpose()
         else:
             return np.random.dirichlet((1,4,2,3), 1).transpose()
-    
+    elif distribution == "4dirichlets":
+        #Sample 1-d policy preference from three dirichlet distributions:
+        coin = np.random.randint(0,4)
+        if coin == 0:
+            return np.random.dirichlet((2,1,3,4), 1).transpose()
+        elif coin == 1:
+            return np.random.dirichlet((3,4,2,1), 1).transpose()
+        elif coin == 2:
+            return np.random.dirichlet((4,1,3,2), 1).transpose()
+        elif coin == 3:
+            return np.random.dirichlet((1,3,2,4), 1).transpose()
+    elif distribution == "magicalBeta":
+        return scipy.stats.beta(nCandidates,1,1)
+    elif distribution == "powerlaw":
+        return np.random.power(3,nCandidates)
+            
 #Function that efficiently checks whether two lists are identical:
 def areIdentical(lhs, rhs):
     result = True
@@ -77,12 +93,12 @@ def printElectResultsAsOfNow(passedCandidates, nElectors):
 #The reason why it is a global function instead of a member function of either
 #Candidates or Electors classes if for performance: then we don't have to make
 #nCandidates x nElectors calculations:
-def countVoteIntentions(passedElectors, passedCandidates):
+def countVoteIntentions(passedElectors, passedCandidates, iteration):
     nCandidates = len(passedCandidates)
     for candidate in passedCandidates:
         candidate.voteIntention = 0
     for elector in passedElectors:
-        chosenCandidate = elector.chooseCandidate(passedCandidates)
+        chosenCandidate = elector.chooseCandidate(passedCandidates, iteration)
         chosenCandidate.voteIntention += 1
     newVoteIntentions = [None] * nCandidates
     index = 0
